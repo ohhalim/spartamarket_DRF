1c6fd4f736b7a88bbc22874336fc0633189d2c97
ohhalim <ohhalim777@gmail.com>
2024-12-28 18:27:56 +0900

    reup

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
     path('profile/', views.profile, name='profile'),  # 회원정보 조회 및 수정
+    path('<str:username>/', views.user_profile, name='user_profile'),    
     path('<int:user_pk>/follow/', views.follow, name='follow'), #팔로우/언팔로우 토
-    path('<str:username>/', views.user_profile, name='user_profile'),
 ]
